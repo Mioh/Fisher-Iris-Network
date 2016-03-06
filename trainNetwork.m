1b2cc8f934559f26f2a48e892e0b4213ed629c70
Mikael <mikael.ohlsson92@gmail.com>
2016-03-06 13:03:11 +0000

    Plot info for all k-folds e.g confusion matrix

--- trainNetwork.m
+++ trainNetwork.m
@@ -6,10 +6,19 @@ function [net, error, errors] = trainNetwork(P, T, k)
     errors = zeros(k,1);
     step = nrSamples/k - 1;
     
+    % Init plot data
+    y = []; % output vector
+    perf = [];
+    vperf = [];
+    gradient = [];
+    best_perf = zeros(k,1);
+    best_vperf = zeros(k,1);
+    
+    % Create network
     net = newff(P, T, 2, {'tansig' 'purelin'});
     net.plotFcns{3}= 'plotconfusion';
     net = init(net);
-    
+   
     % Initiate bounds
     foldEnd = 0;
     for i = 1:k
@@ -24,20 +33,40 @@ function [net, error, errors] = trainNetwork(P, T, k)
         trainInd = 1:nrSamples;
         trainInd(valInd) = []; % remove fold from training
         
-        % Set indicies for training, test and validation (test=validation)
+        % Set indicies for training and validation (test=validation)
         net.divideFcn = 'divideind';
         net.divideParam.trainInd = trainInd;
-        net.divideParam.testInd = valInd;
         net.divideParam.valInd = valInd;
         
         % Train network
         [net, record] = train(net, P, T);
         
+        % Store output
+        y = net(P);
+        
+        % Store plot info
+        perf = [perf record.perf];
+        vperf = [vperf record.vperf];
+        gradient = [gradient record.gradient];
+        best_perf(i, :) = record.best_perf;
+        best_vperf(i, :) = record.best_vperf;
+        
         % Store mean squared error
-        errors(i, :) = record.best_perf;
+        errors(i, :) = record.best_vperf;
+        
         
     end
-    
     % Cross-validation error (avarage mean squared error)
     error = sum(errors) / k;
+    
+    % Plot confition matrix for all folds
+    plotconfusion(T,y);
+    
+    % Plot
+    %figure('Name', 'Performance');
+    %plot(1:length(perf),perf,1:length(perf),vperf);
+    %figure('Name', 'Best Performance');
+    %plot(1:k,best_perf,1:k,best_vperf);
+    %figure('Name', 'Gradient');
+    %plot(gradient);
 end
